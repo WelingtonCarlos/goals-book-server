3aff125e8db4bdbe7a31cdbeff1ecfeb3eaefb8f
WelingtonCarlos <wcfilho98@hotmail.com>
2024-09-17 18:08:48 -0300

    feat? create user and auth module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
